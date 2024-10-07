e6241252ff4c2a13cb27b9ad4ff7274f6eb783a9
Alexandre liberato <alexandreliberatto@gmail.com>
2024-10-07 09:53:09 -0300

    Criando classe Cliente
    Signed-off-by: Alexandre liberato <alexandreliberatto@gmail.com>

---
+++ bancoDigital/src/bancoDigital/Cliente.java
@@ -0,0 +1,46 @@
+package bancoDigital;
+
+public class Cliente {
+	
+	// Atributos
+	String nome;
+	String cpf;
+	String email;
+	
+	// Associa as contas que o cliente possui
+	List<Conta> contas;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Conta> getContas() {
+		return contas;
+	}
+
+	public void setContas(List<Conta> contas) {
+		this.contas = contas;
+	}
+
+	
+}
